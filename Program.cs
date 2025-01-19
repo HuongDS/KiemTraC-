51250021763cc7fb8115e8025f38a80c0b3d4a28
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-01-19 11:55:12 +0700

    commit1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(nums([0]));
+    }
+
+    public static int nums(int[] nums)
+    {
+        nums = nums.OrderBy(x => x).ToArray();
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i + 1] - nums[i] != 1) return nums[i] + 1;
+        }
+        return nums[nums.Length - 1] + 1;
+    }
+}
